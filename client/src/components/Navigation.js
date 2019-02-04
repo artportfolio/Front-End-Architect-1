127e2b1f3ab05a03b767b84614865f20a4654e37
Andrew Benedict <atbenedict@gmail.com>
2019-02-04 14:08:08 -0500

    initial commit

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Navigation = props => {
+  return <div />;
+};
+
+Navigation.propTypes = {};
+
+export default Navigation;
---
+++ client/src/components/PhotoList.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { connect } from "react-redux";
+import { getPhotos } from "../store/actions";
+import PhotoModal from "./PhotoModal";
+import "../styles/photoList.css";
+
+class PhotoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { currentIndex: null };
+    this.closeModal = this.closeModal.bind(this);
+    this.findNext = this.findNext.bind(this);
+    this.findPrev = this.findPrev.bind(this);
+    this.renderImageContent = this.renderImageContent.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.getPhotos();
+  }
+
+  renderImageContent(src, index) {
+    return (
+      <div onClick={e => this.openModal(e, index)}>
+        <img src={src.square} key={src.id} alt="alternative text" />
+      </div>
+    );
+  }
+  openModal(e, index) {
+    this.setState({ currentIndex: index });
+  }
+  closeModal(e) {
+    if (e !== undefined) {
+      e.preventDefault();
+    }
+    this.setState({ currentIndex: null });
+  }
+  findPrev(e) {
+    if (e !== undefined) {
+      e.preventDefault();
+    }
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex - 1
+    }));
+  }
+  findNext(e) {
+    if (e !== undefined) {
+      e.preventDefault();
+    }
+    this.setState(prevState => ({
+      currentIndex: prevState.currentIndex + 1
+    }));
+  }
+  render() {
+    return (
+      <div className="gallery-container">
+        <div className="gallery-grid">
+          {this.props.photos.map(this.renderImageContent)}
+        </div>
+        <PhotoModal
+          closeModal={this.closeModal}
+          findPrev={this.findPrev}
+          findNext={this.findNext}
+          hasPrev={this.state.currentIndex > 0}
+          hasNext={this.state.currentIndex + 1 < this.props.photos.length}
+          src={this.props.photos.square}
+        />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  photos: state.photos
+});
+
+export default connect(
+  mapStateToProps,
+  { getPhotos }
+)(PhotoList);
