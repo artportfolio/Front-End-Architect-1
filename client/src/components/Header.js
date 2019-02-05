52aedd6be7c46277a140e81966f67a86327ebf79
Andrew Benedict <atbenedict@gmail.com>
2019-02-04 19:11:04 -0500

    got rid of ALL of fontawesome

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "../fontawesome";
+
 import "../styles/styles.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = props => {
   return (
     <div>
       <nav className="header">
         <NavLink exact to="/" className="logo">
-          <FontAwesomeIcon className="palette" icon={["fas", "palette"]} />
+          <div className="palette" />
           artful
         </NavLink>
         <NavLink exact to="/" className="login-logout">
